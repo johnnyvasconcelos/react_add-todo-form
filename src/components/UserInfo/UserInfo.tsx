cebb5205f6918af829d1ec1debe7905dcd3a15f6
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-07-28 00:34:40 -0300

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -3,7 +3,7 @@ import { User } from '../../App';
 export const UserInfo = ({ user }: { user: User }) => {
   return (
     <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.email}
+      {user.name}
     </a>
   );
 };
