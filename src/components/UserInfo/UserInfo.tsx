4a8b5d9d8c164a00f11c08adc3b19e573e167a3d
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-07-27 23:45:52 -0300

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,14 +1,15 @@
 import { TodoInterface } from '../../App';
+
 export const UserInfo = ({ todo }: { todo: TodoInterface }) => {
-  const user = todo.user;
+  const user = todo?.user;
 
   if (!user) {
     return null;
   }
 
   return (
-    <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
-      {todo.user?.name}
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
     </a>
   );
 };
